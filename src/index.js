1aa98259127026f9d53a8bbbcbc71ffc2a69b80f
Damien Cassou <damien@cassou.me>
2024-07-26 17:17:20 +0200

    Reproduce bug

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-export function add(number1, number2) {
-  return number1 + number2;
+function main() {
+  let update = makeUpdate();
+
+  update("hello");
 }
+
+function makeUpdate() {
+	let updateFn = () => true;
+
+	fetchItems({
+		setUpdateFn: (fn) => (updateFn = fn),
+	});
+
+	return (...args) => updateFn(...args);
+}
+
+function fetchItems({setUpdateFn}) {
+  setUpdateFn((arg) => console.log(arg));
+}
+
+main()
